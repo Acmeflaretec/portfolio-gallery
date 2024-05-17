3c686127b696745711583a141f74182594f68cfc
abdusamad8575 <samadpts786313@gmail.com>
2024-05-17 13:34:45 +0530

    frontent/Contact.js updated

--- react_portfolio_2/README.md
+++ react_portfolio_2/README.md
@@ -19,14 +19,7 @@ npm run dev
 + aos scroll animation
 + and more....
 
-## youtube video link
-[Watch now](https://youtu.be/YpFK4hUZ-NM)
 
-## live preview link
-[See Now](https://codeaprogram-portfolio.web.app/)
-
-## Startup repository link
-[Repository](https://github.com/Sridhar-C-25/React_portfolio_2_startup)
 
 ## packages:
 + [tailwind css](https://tailwindcss.com/docs/installation)
--- react_portfolio_2/src/Content.js
+++ react_portfolio_2/src/Content.js
@@ -6,15 +6,15 @@ import services_logo1 from "./assets/images/Services/logo1.png";
 import services_logo2 from "./assets/images/Services/logo2.png";
 import services_logo3 from "./assets/images/Services/logo3.png";
 
-import project1 from "./assets/images/projects/img1.png";
-import project2 from "./assets/images/projects/img2.png";
-import project3 from "./assets/images/projects/img3.png";
-import person_project from "./assets/images/projects/person.png";
+import project1 from "./assets/images/Projects/img1.png";
+import project2 from "./assets/images/Projects/img2.png";
+import project3 from "./assets/images/Projects/img3.png";
+import person_project from "./assets/images/Projects/person.png";
 
 import avatar1 from "./assets/images/Hero/a1.png";
 
 import Hireme_person from "./assets/images/Hero/image2-removebg-preview.png";
-
+          
 // import icons from react-icons
 import { GrMail } from "react-icons/gr";
 import { MdArrowForward, MdCall } from "react-icons/md";
